3632e84fe0e2e69c0d77d976df6fce7c8365fd2d
dev-maksimlegit <63642065+dev-maksimlegit@users.noreply.github.com>
2020-05-23 14:28:16 +0300

    Тестовое задание

---
+++ test_project/js/script.js
@@ -0,0 +1,4 @@
+$('.header-menu__burger').click(function() {
+	document.querySelector('.header-menu__burger').classList.toggle('active');
+	document.querySelector('.header-menu__list').classList.toggle('active');
+});
